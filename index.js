43a8b1edad4948e33bd973c0e2f8c161c0734bba
fakebenjay <fakebenjay@gmail.com>
2017-03-22 00:51:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+  Handlebars.registerPartial("repoCommits", $("#commits-template").html())
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, (data) => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail((error) => {
+    displayError()
+  })
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function showCommits(el) {
+  const user = el.dataset.owner
+  const name = el.dataset.repository
+  $.get(`https://api.github.com/repos/${user}/${name}/commits`, (data) => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail((error) => {
+    displayError()
+  })
 }
 
 $(document).ready(function (){
